e062318248c71dfc068019282aca5b62c6bd38f6
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-04-03 16:21:47 +0200

    Feat fatta exception

--- Cibo.php
+++ Cibo.php
@@ -12,45 +12,37 @@ class Cibo extends Prodotto {
         $this->data_di_produzione = $_data_di_produzione;
         $this->data_di_scadenza = $_data_di_scadenza;
     }
-
     public function getDataProduzione() {
         return $this->data_di_produzione;
     }
-
     public function getDataScadenza() {
         return $this->data_di_scadenza;
     }
-
     public function getIngredienti() {
         return $this->ingredienti;
     }
-
     public function setDataProduzione($newValue) {
         $this->data_di_produzione = $newValue;
     }
-
     public function setDataScadenza($newValue) {
         $this->data_di_scadenza = $newValue;
     }
-
     public function setIngredienti($newValue) {
         $this->ingredienti = $newValue;
     }
-
-
     public function printCardCiboHTML() {
     echo"
-            <div class=\"col\">
-                <div style=\"width:300px\" class=\"card\">
-                    <h5 class=\"card-title\"> {$this->getMarca()} </h5>
-                    <img src=\"{$this->getImmagine()}\" class=\"card-img-top\">
-                    <div class=\"card-body\">
-                        <p style=\"font-size:13px\" class=\"card-text\">Prezzo: {$this->getPrezzo()} euro</p>
-                        <p style=\"font-size:13px\" class=\"card-text\">Data di produzione: {$this->getDataProduzione()}</p>
-                        <p style=\"font-size:13px\" class=\"card-text\">Data di scadenza: {$this->getDataScadenza()}</p>
-                        <p class=\"card-text\"><strong>Ingredienti:</strong> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.</p>
-                    </div>
+        <div class=\"col\">
+            <div style=\"width:300px\" class=\"card\">
+                <h5 class=\"card-title\"> {$this->getMarca()} </h5>
+                <img src=\"{$this->getImmagine()}\" class=\"card-img-top\">
+                <div class=\"card-body\">
+                    <p style=\"font-size:13px\" class=\"card-text\">Prezzo: {$this->getPrezzo()} euro</p>
+                    <p style=\"font-size:13px\" class=\"card-text\">Data di produzione: {$this->getDataProduzione()}</p>
+                    <p style=\"font-size:13px\" class=\"card-text\">Data di scadenza: {$this->getDataScadenza()}</p>
+                    <p class=\"card-text\"><strong>Ingredienti:</strong> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.</p>
                 </div>
-            </div>";
-  }
+            </div>
+        </div>";
+    }
 }
--- Prodotto.php
+++ Prodotto.php
@@ -8,10 +8,7 @@ class Prodotto {
     protected $immagine;
     protected $marca;
 
-
     public static $listaCategorie = ["cane", "gatto"];
-
-
 
     function __construct($_nome, $_prezzo, $_tipo, $_categoria,  $_marca, $_immagine){
         $this->nome = $_nome;
@@ -20,45 +17,37 @@ class Prodotto {
         $this->categoria = $_categoria;
         $this->marca = $_marca;
         $this->immagine = $_immagine;
-
     }
-
     public function getNome() {
         return $this->nome;
     }
-
     public function getPrezzo() {
         return $this->prezzo;
     }
-
     public function getTipo() {
         return $this->tipo;
     }
-
     public function getCategoria() {
         return $this->categoria;
     }
-
     public function getImmagine() {
         return $this->immagine;
     }
-
     public function getMarca() {
         return $this->marca;
     }
-
     public function setNome($value) {
         $this->nome = $value;
     }
-
     public function setPrezzo($value) {
+        if($value < 5) {
+            throw new Exception("L'importo non puo' essere inferiore a 5.");
+        }
         $this->prezzo = $value;
     }
-
     public function setTipo($value) {
         $this->tipo = $value;
     }
-
     public function setCategoria($value) {
         $this->categoria = $value;
     }
